2b72fc31005f5be658d065878f3ba8aea4bbcb7a
LeonCookie <LeonGdynia1105@gmail.com>
2022-09-20 19:24:22 +0200

    Dodaj pliki projektów.

---
+++ Sekretariat_v.2.0/Form1.cs
@@ -0,0 +1,15 @@
+namespace Sekretariat_v._2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
